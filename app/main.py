2a403b637956cef69431f482c42bfd324985072b
Tim <tima.ivanov00@gmail.com>
2022-05-11 18:57:50 +0300

    smth

--- app/main.py
+++ app/main.py
@@ -1,13 +1,44 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect
 from flask_login import login_required, current_user
+from .models import *
 
 main = Blueprint('main', __name__)
 
 @main.route('/')
 def index():
-    return render_template('index.html')
+    # Выбрать все из Cards
+    cards = Card.query.all()
+
+    return render_template('index.html', cards=cards)
 
 @main.route('/profile')
 @login_required
 def profile():
-    return render_template('profile.html', name = current_user.name) 
+    return render_template('profile.html', name = current_user.name)
+
+
+@main.route('/view/<int:card_id>')
+def view(card_id):
+    card = Card.query.filter_by(id=card_id).first()
+
+    return render_template('view.html', card=card)
+
+
+@main.route('/newcard')
+@login_required
+def newcard():
+    return render_template('new_card.html')
+
+
+@main.route('/newcard', methods =["POST"])
+@login_required
+def newcard():
+
+    label = request.form['']
+    text= request.form['']
+
+    new_card = Card(text=text, label = label)
+    db.session(new_card)
+    db.sessioncommit()
+
+    return redirect('/')
--- app/models.py
+++ app/models.py
@@ -1,8 +1,17 @@
 from flask_login import UserMixin
 from . import db
+from datetime import datetime
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(100))
+
+class Card(UserMixin, db.Model):
+    card_id = db.Column(db.Integer, primary_key=True)
+    text = db.Column((db.String(500))
+    timestamp = db.Column(db.Datetime, index = True, default= datetime.utcnow)
+    label = db.Column((db.String(40))
+
+
