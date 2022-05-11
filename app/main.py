3b81e00a677b02f3e31869de5f167baabe73a3f4
Kirill <kirill@mail.com>
2022-05-11 20:27:29 +0300

    v0.1

--- app/main.py
+++ app/main.py
@@ -1,13 +1,44 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect
 from flask_login import login_required, current_user
-
+from . import db
+from .models import User, Comment, Product
 main = Blueprint('main', __name__)
 
 @main.route('/')
 def index():
-    return render_template('index.html')
+    products = Product.query.all()
+    return render_template('index.html', products=products)
 
-@main.route('/profile')
+@main.route('/view/<int:product_id>')
+def view(product_id):
+    product = Product.query.filter_by(id=product_id).first()
+    comments = Comment.query.filter_by(product_id=product_id).order_by(Comment.timestamp.desc()).all()
+    return render_template('view.html', product=product, comments=comments)
+
+@main.route('/addcomment', methods=["POST"])
 @login_required
-def profile():
-    return render_template('profile.html', name = current_user.name) 
+def addcomment_post():
+    productId = request.form.get('productId')
+    commentText = request.form.get('commentText')
+    new_comment = Comment(owner_id=current_user.id, product_id=productId, text=commentText)
+    db.session.add(new_comment)
+    db.session.commit()
+    return redirect(f'/view/{productId}')
+
+
+@main.route('/newproduct')
+@login_required
+def newproduct():
+    return ""
+
+
+@main.route('/newproduct', methods=["POST"])
+@login_required
+def newproduct_post():
+    return ""
+
+
+@main.route('/delete', methods=["POST"])
+@login_required
+def delete_post():
+    return ""  
--- app/models.py
+++ app/models.py
@@ -1,8 +1,25 @@
 from flask_login import UserMixin
 from . import db
+from datetime import datetime
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(100))
+    products = db.relationship("Product")
+
+class Product(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    image = db.Column(db.String(100))
+    header = db.Column(db.String(50))
+    description = db.Column(db.String(500))
+    comments = db.relationship("Comment")
+
+class Comment(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'))
+    text = db.Column(db.String(500))
+    timestamp = db.Column(db.Date(), default=datetime.utcnow)
