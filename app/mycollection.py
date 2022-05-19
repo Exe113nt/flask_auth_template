8c6fc40aed42fa60fc6ea4b929b9cb30a0872d8b
s3ctor_priz <brebikhin1095@gmail.com>
2022-05-19 14:20:32 +0300

    098

--- app/mycollection.py
+++ app/mycollection.py
@@ -1,8 +1,21 @@
-@mycollection.route('/view/<int:card_id>')
-def view(card_id):
-    card = Card.query.filter_by(id=card_id).first()
+from flask import Blueprint, render_template, flash, redirect, request, url_for
+from flask_login import login_required, current_user
+from .models import Card
+from . import db
+import uuid
+import os
+from hashlib import md5
+from urllib.request import urlretrieve
+import requests
+import shutil
 
-    return render_template('view.html', card=card)
+
+mycollection = Blueprint('mycollection', __name__) 
+# @mycollection.route('/view/<int:card_id>')
+# def view(card_id):
+#     card = Collection.query.filter_by(id=card_id).first()
+
+#     return render_template('view.html', card=card)
 
 
 @mycollection.route('/newcard')
@@ -29,3 +42,14 @@ def newcard_post():
     db.sessioncommit()
 
     return redirect('/')
+
+
+@cards.route('/showcard/<int:user_id>/<int:card_id>')
+@login_required
+def showcard(user_id, card_id):
+    if current_user.id != user_id:
+        return 505
+    
+    card = Collection.query.filter_by(owner_id=current_user.id, id=card_id).first()
+
+    return render_template('show.html', card=card, id=current_user.id)
